55b1297a1181a92941a556dfb8f49eb81c03bc68
Nariman <nariman.valiyev.97@gmail.com>
2025-05-21 23:34:56 +0500

    settings and include mysql

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,16 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, MySQLDsn
 from pydantic_settings import BaseSettings
 
 class RunConfig(BaseModel):
-    host:str = "0.0.0.0"
+    host:str = "127.0.0.1"
     port: str = 8000
 
 class ApiPrefix(BaseModel):
     api_prefix: str = "/api"
 
+class DataBaseSettings(BaseModel):
+    url:MySQLDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     prefix: ApiPrefix = ApiPrefix()
+    db: DataBaseSettings = DataBaseSettings
 
 
 settings = Settings()
