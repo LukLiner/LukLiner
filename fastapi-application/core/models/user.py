53ae92b0f46c5375c89fc4a1c443713ac18c7704
Nariman <nariman.valiyev.97@gmail.com>
2025-05-25 15:16:37 +0500

    update migration users and settings, add convection

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,7 +1,12 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, UniqueConstraint
 
 from .base import Base
 
 class User(Base):
     username:Mapped[str] = mapped_column(String(100), unique=True)
+    foo: Mapped[int]
+    bar: Mapped[int]
+
+    __table_args__ = (UniqueConstraint('foo','bar'),
+                      )
